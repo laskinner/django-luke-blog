8d8841200439825845762eadbb08e4e6663cb696
Luke Skinner <laskinner@gmail.com>
2023-12-18 01:21:37 +0100

    created procfile and added settings for delpoy

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1%v2)m+nj39-cvt(res78wb*ap!#7)3uwl-7(*afrpe57xax%!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
