45314f8e77470858eeb8aef63f92ba0d6679a526
Luke Skinner <laskinner@gmail.com>
2023-12-19 12:21:24 +0100

    Created superuser, change debug mode back to false

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1%v2)m+nj39-cvt(res78wb*ap!#7)3uwl-7(*afrpe57xax%!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
